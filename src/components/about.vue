4f97d7db42070f393851b094e32058d7bb69f070
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2017-01-05 21:50:49 +0700

    add about & contact components

---
+++ src/components/about.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>About Page</h1>
+
+        <p>
+            <router-link to="/">Index</router-link>
+
+            |
+
+            <router-link :to="{ name: 'contact' }">Contact</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
---
+++ src/components/contact.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Contact Page</h1>
+
+        <p>
+            <router-link to="/">Index</router-link>
+
+            |
+
+            <router-link to="/about">About</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
