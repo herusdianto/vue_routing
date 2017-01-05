5222bebfcadcd6f831a9e16e15e1dd559a6ecd7f
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2017-01-05 22:08:17 +0700

    add author book & user components

---
+++ src/components/authorBook.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Author Book Page</h1>
+
+        <h3>Author ID: {{ $route.params.authorId }}</h3>
+
+        <h4>Book ID: {{ $route.params.bookId }}</h4>
+
+        <p>
+            <router-link to="/">Index</router-link>
+
+            |
+
+            <router-link to="/about">About</router-link>
+
+            |
+
+            <router-link :to="{ name: 'contact' }">Contact</router-link>
+
+            |
+
+            <router-link :to="{ name: 'user', params: { id: 4 } }">User ID 4</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
---
+++ src/components/user.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>User Page</h1>
+
+        <h3>User ID: {{ $route.params.id }}</h3>
+
+        <p>
+            <router-link to="/">Index</router-link>
+
+            |
+
+            <router-link to="/about">About</router-link>
+
+            |
+
+            <router-link :to="{ name: 'contact' }">Contact</router-link>
+
+            |
+
+            <router-link :to="{ name: 'authorBook', params: { authorId: 1, bookId: 4 } }">Author ID 1 & Book ID 4</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
