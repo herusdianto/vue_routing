f4f616421f306c76432bd1808b5769ac34fc9642
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2017-01-05 21:50:34 +0700

    add link to about & contact page

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
         <h1>Index Page</h1>
+
+        <p>
+            <router-link to="/about">About</router-link>
+
+            |
+
+            <router-link :to="{ name: 'contact' }">Contact</router-link>
+        </p>
     </div>
 </template>
 
