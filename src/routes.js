0edf057d069fdefdfd88129fb6bcbea2f2c5ade9
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2017-01-05 21:50:14 +0700

    add about & contact route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,21 @@
 import index from './components/index'
+import about from './components/about'
+import contact from './components/contact'
 
 const routes = [
-    { path: '/', component: index }
+    {
+        path: '/',
+        component: index
+    },
+    {
+        path: '/about',
+        component: about
+    },
+    {
+        path: '/contact',
+        name: 'contact',
+        component: contact
+    },
 ]
 
 export default routes
